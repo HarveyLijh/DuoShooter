af8c342bd5f27e3f4821f384d39e56ef7b06e5bd
Harvey Li <li_jh@yahoo.com>
2021-03-24 23:22:15 -0500

    Adding the basic game made last week

---
+++ Assets/Scripts/Guns/BulletController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+    private float rangeCountDown;
+    public float range;
+    public float damage;
+    public bool isPenetrable;
+    public Vector3 spreadDirection;
+    private Vector3 bulletVanishPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rangeCountDown = range / speed;
+        getDifference(spreadDirection.x, spreadDirection.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rangeCountDown <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            //transform.Translate(spreadDirection * speed * Time.deltaTime);
+            float step = speed * Time.deltaTime;
+            //Debug.DrawLine(transform.position, spreadDirection, Color.red);
+            //Debug.DrawLine(transform.position, bulletVanishPoint, Color.blue);
+            transform.position = Vector3.MoveTowards(transform.position, bulletVanishPoint, step);
+
+            rangeCountDown -= Time.deltaTime;
+
+        }
+    }
+    // calculate the difference between the bulletVanishPoint and the spreadDirection point
+    private void getDifference(float origX, float origZ)
+    {
+        float differenceX = (spreadDirection.x - transform.position.x) * range;
+        float differenceZ = (spreadDirection.z - transform.position.z) * range;
+
+        bulletVanishPoint = new Vector3(spreadDirection.x+ differenceX, spreadDirection.y, spreadDirection.z + differenceZ);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damage);
+        }
+        if (!isPenetrable)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/HurtPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    [SerializeField] float damage;
+    private bool stillHurting;
+
+    private PlayerHealthManager theHealthManager;
+
+    private float nextHurtCountDown;
+    [SerializeField] float nextHurtInterval;
+    //// Start is called before the first frame update
+    void Start()
+    {
+        nextHurtCountDown = nextHurtInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stillHurting)
+        {
+            if (nextHurtCountDown <= 0)
+            {
+                theHealthManager.HurtPlayer(damage);
+                nextHurtCountDown = nextHurtInterval;
+
+            }
+            else
+            {
+                nextHurtCountDown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            nextHurtCountDown = nextHurtInterval;
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            stillHurting = true;
+            theHealthManager = other.gameObject.GetComponent<PlayerHealthManager>();
+            theHealthManager.HurtPlayer(damage);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            stillHurting = false;
+        }
+    }
+}
