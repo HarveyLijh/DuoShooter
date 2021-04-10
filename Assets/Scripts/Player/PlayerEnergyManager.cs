5a1ce17b4a984153072e0ac86402acf848fb6b38
HarveyLijh <li_jh@yahoo.com>
2021-04-10 00:49:11 -0500

    1. modify player healthbar to screen top left 2. remove enemy healthbar 3. add player energy bar, shockwave cost energy, kill enemy adds energy 4. add xp bar and level system, kill enemy add xp 5. add aiming mouse icon 6. add reload progress animation to the aiming mouse icon

---
+++ Assets/Scripts/Player/PlayerEnergyManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    [SerializeField] private float playerHealth;
+    private float currentHealth;
+
+    [SerializeField] private float hurtFlashLength;
+    private float flashCountDown;
+
+    private Renderer rend;
+    private Color origColor;
+
+    [SerializeField]
+    private HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = playerHealth;
+        rend = GetComponent<Renderer>();
+        origColor = rend.material.GetColor("_Color");
+        healthBar.SetMaxHealth(playerHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (flashCountDown <= 0)
+        {
+            rend.material.SetColor("_Color", origColor);
+        }
+        else
+        {
+            flashCountDown -= Time.deltaTime;
+        }
+        if (currentHealth <= 0)
+        {
+            gameObject.GetComponent<Player>().PlayerDead();
+        }
+    }
+
+    public void HurtPlayer(float damageAmount)
+    {
+        flashCountDown = hurtFlashLength;
+        rend.material.SetColor("_Color", Color.red);
+        currentHealth -= damageAmount;
+        healthBar.SetHealth(currentHealth);
+    }
+}
